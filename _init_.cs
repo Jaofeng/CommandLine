9ff3b8d9c19cbe6dbdb76dd750bda449e54efb2a
怪頭 <jaofeng.chen@gmail.com>
2024-06-24 18:35:19 +0800

    Update _init_.cs

--- _init_.cs
+++ _init_.cs
@@ -19,9 +19,28 @@ public delegate bool ExecuteCommandHandler(CliCenter sender, CommandAttribute cm
 /// <summary>定義指令重複的錯誤類別。</summary>
 public class CommandException : Exception
 {
+    /// <summary>取得觸發錯誤的指令類別屬性 <see cref="CommandAttribute"/>。</summary>
+    public CommandAttribute? Command { get; private set; } = null;
+
+    /// <summary>建立新指令的錯誤類別。</summary>
+    /// <param name="cmd">發生錯誤的指令類別。</param>
+    public CommandException(CommandAttribute cmd)
+    {
+        Command = cmd;
+    }
     /// <summary>建立新指令的錯誤類別。</summary>
     /// <param name="msg">錯誤訊息。</param>
-    public CommandException(string msg) : base(msg) { }
+    public CommandException(string msg) : base(msg)
+    {
+        Command = null;
+    }
+    // <summary>建立新指令的錯誤類別。</summary>
+    /// <param name="cmd">發生錯誤的指令類別。</param>
+    /// <param name="msg">錯誤訊息。</param>
+    public CommandException(CommandAttribute cmd, string msg) : base(msg)
+    {
+        Command = cmd;
+    }
 }
 #endregion
 
@@ -29,6 +48,9 @@ public class CommandException : Exception
 /// <summary>定義指令參數的錯誤類別。</summary>
 public sealed class CommandArgumentException : CommandException
 {
+    /// <summary>建立新的指令參數錯誤類別。</summary>
+    /// <param name="cmd">發生錯誤的指令類別。</param>
+    public CommandArgumentException(CommandAttribute cmd) : base(cmd) { }
     /// <summary>建立新的指令參數錯誤類別。</summary>
     /// <param name="msg">錯誤訊息。</param>
     public CommandArgumentException(string msg) : base(msg) { }
@@ -39,9 +61,27 @@ public sealed class CommandArgumentException : CommandException
 /// <summary>定義指令重複的錯誤類別。</summary>
 public sealed class CommandDuplicateException : CommandException
 {
+    /// <summary>取得觸發錯誤的指令執行函示。</summary>
+    public MethodInfo? Method { get; private set; }
+
     /// <summary>建立新的指令重複的錯誤類別。</summary>
     /// <param name="msg">錯誤訊息。</param>
     public CommandDuplicateException(string msg) : base(msg) { }
+    /// <summary>建立新的指令重複的錯誤類別。</summary>
+    /// <param name="cmd">已存在定義的指令類別。</param>
+    /// <param name="method">重複定義的執行函示。</param>
+    public CommandDuplicateException(CommandAttribute cmd, MethodInfo method) : base(cmd)
+    {
+        Method = method;
+    }
+    /// <summary>建立新的指令重複的錯誤類別。</summary>
+    /// <param name="cmd">已存在定義的指令類別。</param>
+    /// <param name="method">重複定義的執行函示。</param>
+    /// <param name="msg">錯誤訊息。</param>
+    public CommandDuplicateException(CommandAttribute cmd, MethodInfo method, string msg) : base(cmd, msg)
+    {
+        Method = method;
+    }
 }
 #endregion
 
